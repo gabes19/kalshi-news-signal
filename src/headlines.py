4b32d9942ac0d2d9520a016dcd8ea3c27cfac1d5
gabes19 <xxc5sg@virginia.edu>
2025-11-06 21:56:42 -0500

    replacing news api with gnews

--- src/headlines.py
+++ src/headlines.py
@@ -3,13 +3,12 @@ import pandas as pd
 import os
 from pathlib import Path
 from dotenv import load_dotenv, find_dotenv
-from newsapi import NewsApiClient
+import requests
 
 #headlines extractions and sentiment classification
 
 load_dotenv()
-news_api_key = os.getenv("NEWS_API_KEY")
-api = NewsApiClient(news_api_key)
+gnews_api_key = os.getenv("GNEWS_API_KEY")
 pipe = pipeline("text-classification", model="tabularisai/multilingual-sentiment-analysis")
 
 #Keywords related to inflation used to identify relevant articles
@@ -17,38 +16,19 @@ keywords = ["cpi", "inflation", "consumer price", "pce", "price index", "ppi",
             "cost of living", "price pressure", "rate hike", "monetary policy",
             "fed", "federal reserve", "interest rate", "disinflation", "deflation"]
 
-#Source IDs of top US news sources by NewsApi (used to filter relevant US inflation news)
-source_ids = ['abc-news', 'al-jazeera-english', 'ars-technica', 'associated-press',
-               'axios', 'bleacher-report', 'bloomberg', 'breitbart-news', 
-               'business-insider', 'buzzfeed', 'cbs-news', 'cnn', 'crypto-coins-news', 
-               'engadget', 'entertainment-weekly', 'espn', 'espn-cric-info', 'fortune',
-                 'fox-news', 'fox-sports', 'google-news', 'hacker-news', 'ign', 
-                 'mashable', 'medical-news-today', 'msnbc', 'mtv-news', 
-                 'national-geographic', 'national-review', 'nbc-news', 
-                 'new-scientist', 'newsweek', 'new-york-magazine', 
-                 'next-big-future', 'nfl-news', 'nhl-news', 'politico',
-                   'polygon', 'recode', 'reddit-r-all', 'reuters', 'techcrunch', 
-                   'techradar', 'the-american-conservative', 'the-hill',
-                 'the-huffington-post', 'the-next-web', 'the-verge',
-                   'the-wall-street-journal', 'the-washington-post',
-                     'the-washington-times', 'time', 'usa-today', 'vice-news', 'wired']
+#TODO: do for gnews
+def get_headlines(sources:list[str],keywords:list[str],from_date:str,to_date:str)->list[str]:
+    keywords_string = ",".join(keywords)
+    print(keywords_string)
+    sources_string = ",".join(sources)
+    print(sources_string)
+    response = api.get_everything(qintitle=keywords_string,sources=sources_string, from_param=from_date, to=to_date, language="en")
+    headlines = []
+    for article in response["articles"]:
+        headlines.append(article["title"])
+    return headlines
+                  
+                  
+                  
 
-articles = []
-
-#TODO
-def get_headlines(sources:list[str],keywords:list[str],day:str)->list[str]:
-    ''' Gets all headlines from specified sources on a given day containing keywords in the title
-    Args: keywords - list of string keywords
-          sources: list of string source ids from NewsApi
-          day - string written in "YYYY-MM-DD" format
-
-    Returns: a list of string headlines on that day
-    '''
-for word in keywords:
-    response = api.get_everything(qintitle=word, language= "en", from_param = "2025-11-04", to = "2025-11-05")
-    articles.append(response)
-if articles:
-    print(articles[0])
-else:
-    print("None..")
 
