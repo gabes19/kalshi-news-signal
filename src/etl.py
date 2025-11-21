01e1960050d204bd6ba36801468729c1863137b3
gabes19 <xxc5sg@virginia.edu>
2025-11-21 17:35:21 -0500

    planning etl kalshi features

--- src/etl.py
+++ src/etl.py
@@ -1,11 +1,12 @@
-#Calculates DAILY LEVEL DATA: date, daily article totals, avg sentiment, kalshi market data
 import pandas as pd
-all_headlines = pd.read_csv('data/headlines/all_headline_data.csv')
-if "Unnamed: 0" in all_headlines.columns:
-    all_headlines = all_headlines.drop(columns=["Unnamed: 0"])
+#Calculates DAILY LEVEL DATA: date, daily article totals, avg sentiment, kalshi market data
+
 
 #Aggregate daily article count, mean sentiment, median sentiment, variance, % pos/neg, min/max sentiment
 def aggregate_daily_headlines(output_file:str):
+    all_headlines = pd.read_csv('data/headlines/all_headline_data.csv')
+    if "Unnamed: 0" in all_headlines.columns:
+        all_headlines = all_headlines.drop(columns=["Unnamed: 0"])
     all_headlines['date'] = all_headlines['date'].astype(str).str.split('T').str[0]
     all_headlines['sentiment'] = (
     all_headlines['sentiment'].astype(str).str.strip("[]").astype(float))
@@ -29,10 +30,28 @@ def aggregate_daily_headlines(output_file:str):
     print(f"File saved as {output_file}")
     return daily_headlines
 
+
+#Creates some features based on kalshi data before exploration
 #Joins cpi, cpi_yoy, and cpi_core kalshi market data to headline data
 #date, market, headline data
+#TODO:
 def join_kalshi_data():
-    return
+    kalshi_df = pd.DataFrame()
+    months = ['jan','feb','mar','apr','may','jun','jul','aug','sep''oct','nov']
+    markets = ['cpi','cpicore','cpiyoy']
+    for market in markets:
+        for month in months:
+            df = pd.read_csv(f'data/{market}/kalshi-price-history-25{month}-day')
+            df['timestamp'] = df['timestamp'].astype(str).str.split('T').str[0]
+            #TODO: Extract column names as thresholds
+            #TODO: Calculate expected_cpi (market-implied mean)
+            #TODO: Calculate daily CPI volatility (std = implied vol)
+            #TODO: Calculate rolling prob volatility (std) for each threshold
+            #TODO: Calculate rolling total change for each threshold
+            #TODO: Calculate slope features (slope_01 = P>0.0 - P>0.1, etc.)
+            #TODO: Calculate tail risk measures (right_tail_share (P>0.4+P>0.5+P>0.6) =  right_tail / P>0.0, e.g.)
+            #TODO: Calculate spread between thresholds
+            df = df.rename(columns={'timestamp':'date'})
 
 
 aggregate_daily_headlines('data/headlines/daily_headlines.csv')
