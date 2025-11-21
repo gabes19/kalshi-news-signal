229a36ad359ceecca4ac57f177b5fe9fad1bd323
gabes19 <xxc5sg@virginia.edu>
2025-11-21 17:00:44 -0500

    Added headline day of week feature

--- src/etl.py
+++ src/etl.py
@@ -1,5 +1,4 @@
-#Calculates DAILY LEVEL DATA: date, daily article totals, avg sentiment
-#Joins headlines and kalshi data and cleans it
+#Calculates DAILY LEVEL DATA: date, daily article totals, avg sentiment, kalshi market data
 import pandas as pd
 all_headlines = pd.read_csv('data/headlines/all_headline_data.csv')
 if "Unnamed: 0" in all_headlines.columns:
@@ -10,7 +9,7 @@ def aggregate_daily_headlines(output_file:str):
     all_headlines['date'] = all_headlines['date'].astype(str).str.split('T').str[0]
     all_headlines['sentiment'] = (
     all_headlines['sentiment'].astype(str).str.strip("[]").astype(float))
-    daily_headlines = all_headlines.groupby('date').agg(
+    daily_headlines = all_headlines.groupby('date', as_index=False).agg(
         headline_count = ('id', 'count'),
         mean_sentiment = ('sentiment', 'mean'),
         median_sentiment = ('sentiment', 'median'),
@@ -23,11 +22,17 @@ def aggregate_daily_headlines(output_file:str):
         unique_sources = ("source", lambda x: list(set(x)))
     )
     daily_headlines = daily_headlines.fillna(0)
+    daily_headlines['date'] = pd.to_datetime(daily_headlines['date'], format='%Y-%m-%d')
+    daily_headlines['day_of_week'] = daily_headlines['date'].dt.weekday
     print(daily_headlines.head())
     daily_headlines.to_csv(output_file, mode='w')
     print(f"File saved as {output_file}")
+    return daily_headlines
 
-
+#Joins cpi, cpi_yoy, and cpi_core kalshi market data to headline data
+#date, market, headline data
+def join_kalshi_data():
+    return
 
 
 aggregate_daily_headlines('data/headlines/daily_headlines.csv')
